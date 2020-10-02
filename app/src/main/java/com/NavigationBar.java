a9a2045c63e5d77dc25e45041b8286ab4a9a80e6
Amindi <amindiperera1@gmail.com>
2020-10-02 20:44:27 +0530

    Navigation Bar added

---
+++ app/src/main/java/com/NavigationBar.java
@@ -0,0 +1,105 @@
+package com;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.example.officialcookit.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.navigation.NavigationView;
+import com.google.android.material.snackbar.Snackbar;
+
+public class NavigationBar extends AppCompatActivity {
+
+    private AppBarConfiguration mAppBarConfiguration;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_navigation_bar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+        final DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        mAppBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow)
+                .setDrawerLayout(drawer)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
+        NavigationUI.setupWithNavController(navigationView, navController);
+
+        navigationView.bringToFront();
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+                switch (menuItem.getItemId()){
+                    case R.id.nav_profile:
+                        Toast.makeText(getApplicationContext(),"Loading your Profile",Toast.LENGTH_LONG).show();
+                        break;
+
+                    case R.id.nav_recipefeed:
+                        Toast.makeText(getApplicationContext(),"Loading Recipe Feed",Toast.LENGTH_LONG).show();
+                        break;
+
+                    case R.id.nav_addpost:
+                        Toast.makeText(getApplicationContext(),"Loading add posts",Toast.LENGTH_LONG).show();
+                        break;
+
+                    case R.id.nav_walk:
+                        Toast.makeText(getApplicationContext(),"Starting to count steps",Toast.LENGTH_LONG).show();
+                        break;
+
+                    case R.id.nav_measure:
+                        Toast.makeText(getApplicationContext(),"Calculating",Toast.LENGTH_LONG).show();
+                        break;
+
+                    case R.id.nav_logout:
+                        Toast.makeText(getApplicationContext(),"Loging out",Toast.LENGTH_LONG).show();
+                        break;
+                }
+
+                drawer.closeDrawers();
+
+                return false;
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        return NavigationUI.navigateUp(navController, mAppBarConfiguration)
+                || super.onSupportNavigateUp();
+    }
+}
