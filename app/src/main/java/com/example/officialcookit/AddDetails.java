1acc4b4420ea744eb8fba0204e92359bcb034eb1
Amindi <amindiperera1@gmail.com>
2020-10-03 21:14:09 +0530

    edited

--- app/src/main/java/com/example/officialcookit/AddDetails.java
+++ app/src/main/java/com/example/officialcookit/AddDetails.java
@@ -1,22 +1,21 @@
 package com.example.officialcookit;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-
 import com.google.android.material.textfield.TextInputLayout;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+
 public class AddDetails extends AppCompatActivity {
 
     //variables
-    TextInputLayout addUserName,addEmail,addLocation,addPassword;
+    TextInputLayout addUserName, addEmail, addLocation, addPassword;
     Button addDetails;
 
-    FirebaseDatabase  rootNode;
+    FirebaseDatabase rootNode;
     DatabaseReference reference;
 
     @Override
@@ -31,28 +30,26 @@ public class AddDetails extends AppCompatActivity {
         addPassword = findViewById(R.id.Password);
         addDetails = findViewById(R.id.addDetailsSubmit);
 
+
         //save data in firebase on button click
-        addDetails.setOnClickListener(new View.OnClickListener(){
+        addDetails.setOnClickListener(new View.OnClickListener() {
+
             @Override
-            public void onClick(View view){
+            public void onClick(View view) {
+
                 rootNode = FirebaseDatabase.getInstance();
-                reference= rootNode.getReference("Users");
+                reference = rootNode.getReference("Users");
 
                 //Get all the values
                 String username = addUserName.getEditText().getText().toString();
                 String email = addEmail.getEditText().getText().toString();
                 String location = addLocation.getEditText().getText().toString();
                 String password = addPassword.getEditText().getText().toString();
-
-                UserHelperClass helperclass = new UserHelperClass(username,email,location,password);
-
+                UserHelperClass helperclass = new UserHelperClass(username, email, location, password);
                 reference.child(username).setValue(helperclass);
-
             }
-
 
         });//Add details method end
         //onCreate method end
     }
-
 }
--- app/src/main/java/com/example/officialcookit/User_Profile.java
+++ app/src/main/java/com/example/officialcookit/User_Profile.java
@@ -2,13 +2,40 @@ package com.example.officialcookit;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class User_Profile extends AppCompatActivity {
+
+    TextView userName, email, location, password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user__profile);
+
+        //hooks
+        userName = findViewById(R.id.UserName);
+        email = findViewById(R.id.Email);
+        location = findViewById(R.id.Location);
+        password = findViewById(R.id.Password);
+
+        //ShowAllData
+        showUserData();
     }
+
+    private void showUserData() {
+        Intent intent = getIntent();
+        String user_username = intent.getStringExtra("User Name");
+        String user_email = intent.getStringExtra("Email");
+        String user_location = intent.getStringExtra("Location");
+        String user_password = intent.getStringExtra("Password");
+
+        userName.getEditText().setText(user_username);
+        email.getEditText().setText(user_email);
+        location.getEditText().setText(user_location);
+        password.getEditText().setText(user_password);
+    }
+
 }
