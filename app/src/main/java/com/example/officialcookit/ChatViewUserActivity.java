5b742d396c80b20d19dd9e297c652a3c5ca41e0e
Anubis-CMYK <it19099132@my.sliit.lk>
2020-10-04 00:23:27 +0530

    ChatViewUserActivity add

---
+++ app/src/main/java/com/example/officialcookit/ChatViewUserActivity.java
@@ -0,0 +1,14 @@
+package com.example.officialcookit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ChatViewUserActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat_view_user);
+    }
+}
